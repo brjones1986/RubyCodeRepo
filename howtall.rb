ac2711726fc72206157947a27ae3ff7eda1ab27f
brjones1986 <brjones1986@gmail.com>
2015-04-30 00:07:44 +0000

    nursery rhyme madlib

---
+++ howtall.rb
@@ -0,0 +1,8 @@
+puts 'How tall are you in inches?'
+puts''
+height_inches = gets.chomp.to_f
+puts 'your height in inches is ' + height_inches
+
+
+height_cent = height_inches * 2
+puts 'your height in centimeters is ' + height_cent
